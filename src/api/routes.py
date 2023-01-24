376627b484d64e3a226b87f81e25959c9363c7f0
INVIiICTUS <j.enriquealorenzo@gmail.com>
2023-01-24 18:51:14 +0000

    revisar a fondo

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,13 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -13,6 +17,59 @@ def handle_hello():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def handle_user():
+    data = request.data
+    data = json.loads(data)
+
+    user = User(email = data['email'], password = data['password'], is_active=True)
+
+    # email_exists = request.json.get("email", None)
+    # user_created = User.query.filter_by(email=email_exists).first()
+    # if user_created == email_exists:
+    #     return jsonify({"msg": "El usuario ya existe"})
+
+    db.session.add(user)
+    db.session.commit()
+
+    response_body = {
+        "email": data['email'],
+        "password": data['password'],
+        "ok": "true"
+    }
+
+    return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user == None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=user.email)
+    response_body = {
+        "message": "Token created",
+        "token": access_token
+    }
+    
+    return jsonify(response_body), 200
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def handle_private():
+    
+    response_body = {
+        "message": "This is a private route",
+        "ok": "true",
+        "user": get_jwt_identity()
     }
 
     return jsonify(response_body), 200
